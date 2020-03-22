5ec1af4c6f34fe8e88f5909746db8fdd55695c13
tramanjaneyareddy <58390107+tramanjaneyareddy@users.noreply.github.com>
2020-03-22 23:06:20 +0530

    settings file in change the debug mode

--- aws/settings.py
+++ aws/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1fe581ao&vxw6%ad4!qo2t@rcvzn3k=by0rd524$gf#*r$!54g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
